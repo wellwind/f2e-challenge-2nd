03460ff4109ad6ccfef9b2090b370ee5f99b1006
黃升煌 Mike <mike@miniasp.com>
2019-07-08 21:39:37 +0800

    第一頁基本 layout

--- week01-pomodoro/src/app/app.module.ts
+++ week01-pomodoro/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
