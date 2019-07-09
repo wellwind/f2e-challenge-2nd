5a0a8f586f1ea1864a8242b770f8a8028b608b7a
黃升煌 Mike <mike@miniasp.com>
2019-07-09 23:14:24 +0800

    加入時鐘功能
    ref: https://jsfiddle.net/upsidown/e6dx9oza/

--- week01-pomodoro/src/app/main/main.component.ts
+++ week01-pomodoro/src/app/main/main.component.ts
@@ -6,12 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  degree = 180;
 
-  degree = 36;
+  c = 270;
+  r = 270;
 
-  constructor() { }
+  svgPathDefine = '';
+  constructor() {}
 
   ngOnInit() {
+    setInterval(() => {
+      this.degree += 10;
+      this.degree = this.degree % 360;
+      this.svgPathDefine = this.describeArc(this.c, this.c, this.r, 0, this.degree);
+    }, 500);
   }
 
+  polarToCartesian(centerX: number, centerY: number, radius: number, angleInDegrees: number) {
+    const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+    return {
+      x: centerX + radius * Math.cos(angleInRadians),
+      y: centerY + radius * Math.sin(angleInRadians)
+    };
+  }
+
+  describeArc(x: number, y: number, radius: number, startAngle: number, endAngle: number) {
+    const start = this.polarToCartesian(x, y, radius, endAngle);
+    const end = this.polarToCartesian(x, y, radius, startAngle);
+
+    const arcSweep = endAngle - startAngle <= 180 ? '0' : '1';
+
+    const d = ['M', start.x, start.y, 'A', radius, radius, 0, arcSweep, 0, end.x, end.y, 'L', x, y, 'L', start.x, start.y].join(' ');
+
+    console.log(d);
+
+    return d;
+  }
 }
