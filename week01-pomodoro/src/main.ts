2215adbffd5f31b4965dfe3db41538c47002e0aa
wellwind <wellwind@mail2000.com.tw>
2019-07-22 21:42:11 +0800

    (workaround) 手動註冊 service worker

--- week01-pomodoro/src/main.ts
+++ week01-pomodoro/src/main.ts
@@ -1,13 +1,19 @@
-import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import 'hammerjs';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(_ => {
+    if ('serviceWorker' in navigator && environment.production) {
+      navigator.serviceWorker.register('/ngsw-worker.js');
+    }
+  })
   .catch(err => console.error(err));
