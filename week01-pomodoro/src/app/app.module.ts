9108fd1108f4e28c32c0bdd0d0aa42cc3a03ab7d
Mike Huang <wellwind@mail2000.com.tw>
2019-07-22 18:48:09 +0800

    安裝 @angular/pwa

--- week01-pomodoro/src/app/app.module.ts
+++ week01-pomodoro/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { MainComponent } from './main/main.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { SettingsComponent } from './settings/settings.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, MainComponent, TodoListComponent, SettingsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, FlexLayoutModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, FlexLayoutModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
